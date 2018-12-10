52156d8f71cf531cc613c3936ceb3f0d413b569a
pstevenson <paul.stevenson@telethonkids.org.au>
2018-12-10 10:25:00 +0800

    updated R/group_interval.R new logic now a full comparison of all intervals and grouping via igraph

--- R/group_interval.R
+++ R/group_interval.R
@@ -1,25 +1,26 @@
-#' Group by Interval
+#' Group by date interval
 #'
-#' Group records in a data frame by date intervals and proximity
+#' Group records in a data frame by date that intervals including a buffer period.
 #'
-#' @param x the data frame to be grouped, must be sorted by start date column
-#' @param b the buffer interval i.e . /{lubridate::days(14)}
+#' @param dat the data frame to be grouped, must be sorted by start date column
+#' @param buffer the buffer interval e.g. days(14)
 #' @param start,end the column name (as a character) containing the records'
 #'   start and end dates, respectively
-#' @param debug passing debug = T (default is F) will mutate the input data frame
-#'   with the group number and intermediate calculated value and write to the
-#'   global variable group_interval_checking (not active)
+#' @param .view_mat when TRUE (defult FALSE) creates an overlap matrix and applies
+#'   View()
+#' @param .view_graph when TRUE (default FALSE) plots the graph of crouped records
 #'
 #' @return an integer vector of group numbers
 #'
 #' @import dplyr
-#' @importFrom magrittr %>%
-#' @import tidyr
-#' @importFrom data.table shift
-#' @importFrom lubridate interval as.duration
+#' @importFrom igraph components graph_from_data_frame
+#' @importFrom lubridate interval
+#' @importFrom tidyr spread
 #'
 #' @examples
+#'
 #' \dontrun{
+#'
 #' dat <- data_frame(
 #'   name = c("Paul", "Jess", "Ben", "Josie", "Damien", "Hayley"),
 #'   start = ymd(c("2018-05-25", "2018-05-29", NA, "2018-06-25",
@@ -30,39 +31,55 @@
 #' dat <- arrange(dat, start, end)
 #'
 #' group_interval(dat, days(2), "start", "end")
+#'
 #' }
 #'
 #' @export
-group_interval <- function(x, b, start, end, debug = F) {
-  # Offset start date and
-  x[[end]] <- apply(x, 1, function(y) ifelse(is.na(y[[end]]), y[[start]], y[[end]]))
-  next_adm <- shift(x[[start]], n = 1L, fill = NA, type = "lead")
+group_interval <- function(dat, buffer, start, end, .view_mat = F, .view_graph = F) {
 
-  # calculate interval between records
-  record_interval = interval(x[[end]], next_adm) %>%
-    as.duration() %>% as.numeric("seconds")
+  dat[is.na(dat[[start]]) & is.na(dat[[end]]), ] <- NULL # remove row if both adm and sep is NA
+  dat[is.na(dat[[start]]), start] <- dat[is.na(dat[[start]]), end] # set adm == sepa if missing adm
+  dat[is.na(dat[[end]]), end] <- dat[is.na(dat[[end]]), start] # set sepa == adm if missing sepa
+  dat[["interval_lower"]] <- dat[[start]] - buffer # start interval is y before adm date
+  dat[["interval_upper"]] <- dat[[end]] + buffer # end interval is y after adm date
 
-  # put everything in data frame
-  d <- data_frame(record_interval = record_interval, buffer = b %>%
-                    as.duration() %>% as.numeric("seconds"))
+  dat <- dat %>%
+    mutate(rid = 1:nrow(dat),# add in row id
+           intervals = interval(interval_lower, interval_upper)) # define admissions as intervals incorporating buffer
 
-  # determine if interval is greater than allowable difference
-  o <- ifelse(d[["record_interval"]] <= d[["buffer"]], F, T)
+  df_overlap <- bind_cols(
+    expand.grid(dat$rid, dat$rid), # make a 2 col table with every combination of id numbers
+    expand.grid(dat$intervals, dat$intervals)) %>% # make a combination of every interval
+    mutate(overlap = int_overlaps(Var11, Var21)) %>% # determine if intervals overlap
+    rename("row" = "Var1", "col" = "Var2")
 
-  # determine group number(s)
-  o[is.na(o)] <- T
+  # Find groups via graph theory See igraph package
+  dat_graph <- graph_from_data_frame(df_overlap[df_overlap[["overlap"]], c("row", "col")])
+  groups <- components(dat_graph)$membership[df_overlap$row]
 
-  # apply group numbers
-  output <- c(1, 1 + cumsum(o))[1:length(o)] %>% as.integer()
+  # create a 2 column df with row (index) and group number, arrange on row number and return distinct values
+  df_groups <- data_frame(row = as.integer(names(groups)), group = groups) %>%
+    arrange(row) %>%
+    distinct()
 
-  # error checking
-  d <- mutate(d,
-              adm = x[[start]], sepa = x[[end]], next_adm = next_adm,
-              interval_as_date = interval(x[[end]], next_adm) %>% as.duration(),
-              same_next = o, group_no = output)
+  # View df
+  if (.view_mat) {
+    df_view <- df_overlap %>%
+      select(row, col, overlap) %>%
+      spread(key = col, value = overlap) %>% # turn into a n x n data_frame
+      select(-row)
+    df_view[lower.tri(df_view, diag = F)] <- F # set lower triangle to FALSE (keep diagonals as TRUE)
+    apply(df_view, 2, function(x) ifelse(!x, NA, x)) %>% # change FALSE to NA
+      View()
+  }
 
-  # if (debug == T) group_interval_checking <- d[, c("adm", "sepa", "next_adm", "record_interval", "buffer", "interval_as_date", "same_next", "group_no")]
+  # View graph
+  if (.view_graph) {
+    plot(dat_graph, layout = layout_with_fr, vertex.size = 4,
+         vertex.label.dist = 0.5, vertex.color = "red", edge.arrow.size = 0.5)
+  }
 
-  d[["group_no"]]
+  # returns
+  df_groups$group
 
 }
