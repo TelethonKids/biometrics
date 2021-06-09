46eefc51a834ef79d625e6185e24edb54dff990e
paul.stevenson@telethonkids.org.au <paul.stevenson@telethonkids.org.au>
2021-06-09 12:43:56 +0800

    Updates to clean_REDCap to handle dates better

--- R/clean_REDCap.R
+++ R/clean_REDCap.R
@@ -123,6 +123,131 @@ variable_labels <- function(d, dict) map_chr(names(d),
                                                      dict[dict$`Variable / Field Name` == .x,]$`Field Label`,
                                                      .x), dict = dict)
 
+# clean_REDCap.R
+# Helper functions to clean imported REDCap data per the REDCap data dictionary
+# Cretaed by Paul Stevenson, 2021-05-14
+
+#' factor_table
+#'
+#' @param x character vector of REDCap options (e.g. 0, No | 1, Yes)
+#' @param rows (default "\\|") Regex defining row separator
+#' @param cols (default ",") Regex defining column separator
+#'
+#' @return 2 column tibble of factor levels ("key") and labels ("value")
+#'
+#' @importFrom tibble tibble
+#' @importFrom stringr str_split
+#' @importFrom tidyr separate
+#' @importFrom dplyr mutate across
+#' @importFrom tidyselect everything
+#'
+#' @examples
+#' \dontrun{ factor_table("0, No | 1, Yes") }
+#'
+#' @export
+factor_table <- function(x, rows = "\\|", cols = ",") {
+
+  key <- NULL
+
+  tibble(key = str_split(x, rows)[[1]]) %>%
+    separate(key, into = c("key", "value"), sep = cols, extra = "merge") %>%
+    mutate(across(everything(), trimws))
+
+}
+
+#' factor_convert
+#'
+#' Apply factor labels to categorical responses per REDCap data dictionary.
+#'
+#' @param x character vector
+#' @param d REDCap import
+#' @param dict REDCap data dictionary
+#'
+#' @importFrom dplyr cur_column
+#'
+#' @examples
+#' \dontrun{
+#'
+#' dat <- tibble(var = c("0", "1"))
+#'
+#' dictionary <- tibble(`Variable / Field Name` = "var",
+#'   `Choices, Calculations, OR Slider Labels` = "0, No | 1, Yes")
+#'
+#' mutate(dat, across("var", factor_convert, d = dat, dict = dictionary))
+#'
+#' }
+#'
+#' @export
+factor_convert <- function(x, d, dict) {
+  y <- factor_table(dict[dict$`Variable / Field Name` == cur_column(),]$`Choices, Calculations, OR Slider Labels`)
+  factor(x, levels = y$key, labels = y$value)
+}
+
+#' checkbox_labels
+#'
+#' Apply factor labels to categorical responses (checkboxes) per REDCap data dictionary
+#'
+#' @param x checkbox `Variable / Field Name` per data dictionary
+#' @param dict REDCap data dictionary
+#'
+#' @return Named list of character objects
+#'
+#' @importFrom dplyr cur_column
+#'
+#' @examples
+#' \dontrun{
+#'
+#' dat <- tibble(var___0 = c("0", "1"), var___1 = c("1", "0"))
+#'
+#' dictionary <- tibble(`Variable / Field Name` = "var",
+#'   `Choices, Calculations, OR Slider Labels` = "0, No | 1, Yes")
+#'
+#' dat <- labelled::set_variable_labels(dat, .labels = checkbox_labels("var", dictionary))
+#'
+#' }
+#'
+#' @export
+checkbox_labels <- function(x, dict) {
+
+  d <- factor_table(dict[dict$`Variable / Field Name` == x,]$`Choices, Calculations, OR Slider Labels`) %>%
+    mutate(across("key", ~paste(x, ., sep = "___")))
+
+  o <- as.list(d$value)
+  names(o) <- d$key
+
+  o
+}
+
+#' variable_labels
+#'
+#' Apply variable labels to a data frame per the REDCap data dictionary. If
+#' there is no variable label associated with a column then is lable will be the
+#' same as the column's name.
+#'
+#' @param d REDCap import
+#' @param dict REDCap data dictionary
+#'
+#' @return character vector (length `ncol(d)`) of REDCap Field Labels per the data dictionary
+#'
+#' @importFrom purrr map_chr
+#'
+#' @examples
+#' \dontrun{
+#'
+#' dat <- tibble(var = 1)
+#'
+#' dictionary <- tibble(`Variable / Field Name` = "var", `Field Label` = "Label")
+#'
+#' dat <- labelled::set_variable_labels(dat, .labels = variable_labels(dat, dictionary))
+#'
+#' }
+#'
+#' @export
+variable_labels <- function(d, dict) map_chr(names(d),
+                                             ~ifelse(.x %in% dict$`Variable / Field Name`,
+                                                     dict[dict$`Variable / Field Name` == .x,]$`Field Label`,
+                                                     .x), dict = dict)
+
 #' clean_REDCap
 #'
 #' Clean a REDCap extract by applyly factor levels and convert column classes
@@ -134,13 +259,13 @@ variable_labels <- function(d, dict) map_chr(names(d),
 #'
 #' @return cleaned data frame
 #'
-#' @importFrom stringr str_replace
+#' @importFrom stringr str_replace str_detect
 #' @importFrom dplyr mutate across
 #' @importFrom magrittr `%>%`
 #' @importFrom tidyselect starts_with
 #' @importFrom purrr map_lgl
 #' @importFrom labelled set_variable_labels
-#' @importFrom lubridate dmy mdy ymd dmy_hm mdy_hm ymd_hm dmy_hms mdy_hms ymd_hms
+#' @importFrom lubridate ymd ymd_hm ymd_hms hm ms
 #' @importFrom janitor excel_numeric_to_date
 #'
 #' @examples
@@ -177,15 +302,11 @@ clean_REDCap <- function(d, dict, numeric_date = FALSE) {
     d <- mutate(d, across(dict[grepl("^date.*$", dict$`Text Validation Type OR Show Slider Number`, perl = TRUE),]$`Variable / Field Name`, ~excel_numeric_to_date(as.numeric(.))))
   } else {
     d <- mutate(d,
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "date_dmy", isTRUE),]$`Variable / Field Name`, dmy),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "date_mdy", isTRUE),]$`Variable / Field Name`, mdy),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "date_ymd", isTRUE),]$`Variable / Field Name`, ymd),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_dmy", isTRUE),]$`Variable / Field Name`, dmy_hm),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_mdy", isTRUE),]$`Variable / Field Name`, mdy_hm),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_ymd", isTRUE),]$`Variable / Field Name`, ymd_hm),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_seconds_dmy", isTRUE),]$`Variable / Field Name`, dmy_hms),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_seconds_mdy", isTRUE),]$`Variable / Field Name`, mdy_hms),
-                across(dict[map_lgl(dict$`Text Validation Type OR Show Slider Number` == "datetime_seconds_ymd", isTRUE),]$`Variable / Field Name`, ymd_hms))
+                across(dict[map_lgl(str_detect(dict$`Text Validation Type OR Show Slider Number`, "^date_"), isTRUE),]$`Variable / Field Name`, ymd),
+                across(dict[map_lgl(str_detect(dict$`Text Validation Type OR Show Slider Number`, "^datetime_(?!seconds_)"), isTRUE),]$`Variable / Field Name`, ymd_hm),
+                across(dict[map_lgl(str_detect(dict$`Text Validation Type OR Show Slider Number`, "^datetime_seconds_"), isTRUE),]$`Variable / Field Name`, ymd_hms),
+                across(dict[map_lgl(str_detect(dict$`Text Validation Type OR Show Slider Number`, "^time$"), isTRUE),]$`Variable / Field Name`, hm),
+                across(dict[map_lgl(str_detect(dict$`Text Validation Type OR Show Slider Number`, "^time_mm_ss$"), isTRUE),]$`Variable / Field Name`, ms))
   }
 
   d <- set_variable_labels(d, .labels = variable_labels(d, dict))
