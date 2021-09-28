da05426c28630c6571531bd7a4e289ed294a8d7f
Wesley Billingham <wesley.billingham@telethonkids.org.au>
2021-09-28 14:38:11 +0800

    Add roundf function

---
+++ R/roundf.R
@@ -0,0 +1,17 @@
+#' roundf
+#'
+#' A function to keep trailing zeroes when rounding numbers.
+#'
+#' @param x a numeric vector
+#' @param digits integer indicating the number of decimal places to be used.
+#'
+#' @return object of class character"
+#'
+#' @examples
+#' roundf(mtcars$wt, 2)
+#'
+#' @export
+
+roundf <- function(x, digits = 0){
+  sprintf(paste0("%.",digits,"f"), round(x,digits))
+}
