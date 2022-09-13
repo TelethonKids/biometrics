a2bf16035aeee3d489e8eaf97098adacafa9a330
wesbillingham <wesbillingham@gmail.com>
2022-09-14 07:57:18 +0800

    Add redcap functions

---
+++ R/redcap_functions.R
@@ -0,0 +1,29 @@
+#' create_REDCap_dictionary
+#'
+#' A function to generate a data dictionary for a REDCap dataset.
+#'
+#'
+#' @param x a dataframe with REDCap column names retained.
+#' @param dictionary a REDCap dictionary (extracted from REDCap with standard formatting)
+#'
+#' @return object of class dataframe"
+#'
+#' @examples
+#' questionnaire_dictionary <- exportMeta(redcap_connection)
+#' create_REDCap_dictionary(questionnaire_year1, questionnaire_dictionary)
+#'
+#'
+#' @export
+
+create_REDCap_dictionary <- function(x, dictionary) {
+
+  names <- unique(str_replace_all(colnames(x) , "___\\d+$", ""))
+
+  # Filtered Dictionary
+  output <- dictionary %>%
+    filter(`Variable / Field Name` %in% names) %>%
+    arrange(factor(`Variable / Field Name`, levels = names)) %>%
+    mutate(across(everything(), ~ifelse(is.na(.), "", as.character(.))))
+
+  output
+}
